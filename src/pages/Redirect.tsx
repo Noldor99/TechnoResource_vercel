b944eafe422dda2b8b1818ab233bc02392dddacd
Noldor99 <qwerr19942017@gmail.com>
2024-06-12 21:29:22 +0300

    simple

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -1,7 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Redirect = () => {
-  return <div>Redirect</div>
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate("/TechnoResource")
+  }, [navigate])
+
+  return <div>Redirecting...</div>
 }
 
 export default Redirect
